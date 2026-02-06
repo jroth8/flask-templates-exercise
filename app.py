9c437a7e5a6ec7d2e08928f545614ea0eca0f537
Jacob Roth <jroth8@asu.edu>
2026-02-06 10:42:35 -0700

    Initial Web Page
    Initial Commit - added app.py, base template, and home page.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
